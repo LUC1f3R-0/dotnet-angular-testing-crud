97f3818bc7cfeb4df4ecfcf00d7cf085dddc60c3
LUC1f3R-0 <thusharathilan123@gmail.com>
2026-08-08 21:54:46 +0530

    updated

--- front-end/src/app/home/home.ts
+++ front-end/src/app/home/home.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
+  
 export class Home implements OnInit{
 
   isDisabled = false;
+
+  crudApplication;
+  constructor(private fb: FormBuilder) {
+    this.crudApplication = this.fb.group({
+      firstName: ['', [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(50)
+      ]],
+      lastName: ['', [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(50)
+      ]],
+      email: ['', [
+        Validators.email,
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(100)
+      ]],
+      age: ['', [
+        Validators.required,
+      ]]
+    })
+  }
+
+  onSubmitCrud() { 
+    console.log(this.crudApplication.value);
+    if (this.crudApplication.invalid) {
+      return;
+    }
+  }
   
   user = {
     name: 'thushara',
